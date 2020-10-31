2d44172f8a21390f804175294700248895dd41e8
tranha31 <tranquangha31082000@gmail.com>
2020-11-01 00:53:12 +0700

    new gg sheet

---
+++ TetThieuNhi/src/main/java/TetThieuNhi/Main.java
@@ -0,0 +1,16 @@
+
+package TetThieuNhi;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        TetThieuNhi.TruyCapSheet();
+        CuoiNam.TruyCapSheet();
+        TongKetNam.TruyCapSheet();
+        
+   }   
+}
